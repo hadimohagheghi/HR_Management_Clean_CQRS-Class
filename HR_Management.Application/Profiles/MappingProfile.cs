15262b561b1d58332cd0f39b2a25144208b018e2
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-05 16:58:29 +0330

    ساخت Dto ها و Map کردن آنها در Profile

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using HR_Management.Application.DTOs.LeaveAllocation;
+using HR_Management.Application.DTOs.LeaveRequest;
+using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Domain;
 
 namespace HR_Management.Application.Profiles
 {
@@ -6,7 +10,9 @@ namespace HR_Management.Application.Profiles
     {
         public MappingProfile()
         {
-
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
         }
     }
 }

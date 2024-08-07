edc8f81b2ae57690d7debfc72b51f11b6541a499
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-07 18:17:35 +0330

    تکمیل Query ها برای MediatR - بخش اول

---
+++ HR_Management.Application/Features/LeaveAllocations/Handlers/Queries/GetLeaveAllocationDetailRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR_Management.Application.DTOs.LeaveAllocation;
+using HR_Management.Application.Features.LeaveAllocations.Requests.Queries;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using HR_Management.Application.Persistence.Contracts;
+
+namespace HR_Management.Application.Features.LeaveAllocations.Handlers.Queries
+{
+    public class GetLeaveAllocationDetailRequestHandler : IRequestHandler<
+        GetLeaveAllocationDetailRequest, LeaveAllocationDto
+        >
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveAllocationDetailRequestHandler(ILeaveAllocationRepository leaveAllocationRepository,
+            IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<LeaveAllocationDto> Handle(GetLeaveAllocationDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository
+                .GetLeaveAllocationWithDetails(request.Id);
+            return _mapper.Map<LeaveAllocationDto>(leaveAllocation);
+        }
+    }
+}

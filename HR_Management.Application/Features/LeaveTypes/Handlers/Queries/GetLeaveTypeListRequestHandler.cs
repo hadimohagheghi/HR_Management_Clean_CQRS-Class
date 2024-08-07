727400385c3f419ddf0918927050df13792dc231
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-07 18:03:36 +0330

    پیاده سازی الگوی MediatR در Asp.Net Core

---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Requests.Queries;
+using HR_Management.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HR_Management.Application.Persistence.Contracts;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler :
+        IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

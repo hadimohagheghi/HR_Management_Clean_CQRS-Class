d1b067caa2cbae192dd6f75a269ef836d23bcd91
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-07 18:09:18 +0330

    ساخت و تکلمیل Handler ها

---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeDetailRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using HR_Management.Application.Persistence.Contracts;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeDetailRequestHandler : IRequestHandler<
+        GetLeaveTypeDetailRequest, LeaveTypeDto
+        >
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeDetailRequestHandler(ILeaveTypeRepository leaveTypeRepository,
+            IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<LeaveTypeDto> Handle(GetLeaveTypeDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            return _mapper.Map<LeaveTypeDto>(leaveType);
+        }
+    }
+}
--- HR_Management.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -1,15 +1,12 @@
 ﻿using AutoMapper;
 using HR_Management.Application.DTOs.LeaveType;
 using HR_Management.Application.Features.LeaveTypes.Requests.Queries;
-using HR_Management.Application.Contracts.Persistence;
+using HR_Management.Application.Persistence.Contracts;
 using MediatR;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using HR_Management.Application.Persistence.Contracts;
 
 namespace HR_Management.Application.Features.LeaveTypes.Handlers.Queries
 {
@@ -17,9 +14,20 @@ namespace HR_Management.Application.Features.LeaveTypes.Handlers.Queries
         IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
     {
 
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository
+            , IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
         public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var leaveTypeList = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDto>>(leaveTypeList);
         }
     }
 }

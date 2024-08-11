866b24bc3a641f6158f9b020517785ca57c8fd80
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-11 17:13:16 +0330

    تکمیل DTO ها

---
+++ HR_Management.Application/DTOs/LeaveType/CreateLeaveTypeDto.cs
@@ -0,0 +1,13 @@
+﻿using HR_Management.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Application.DTOs.LeaveType
+{
+    public class CreateLeaveTypeDto 
+    {
+        public string Name { get; set; }
+        public int DefaultDay { get; set; }
+    }
+}

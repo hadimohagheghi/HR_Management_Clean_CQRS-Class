5d18ef55a74efa28ad637fa109c6a568e2cb719d
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-14 17:17:56 +0330

    اعتبارسنجی ایجاد درخواست جدید

---
+++ HR_Management.Application/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace HR_Management.Application.DTOs.LeaveRequest.Validators
+{
+    public class CreateLeaveRequestDtoValidator: AbstractValidator<CreateLeaveRequestsDto>
+    {
+        public CreateLeaveRequestDtoValidator()
+        {
+            RuleFor(p => p.StartDate)
+                .LessThan(p => p.EndDate)
+                .WithMessage("{PropertyName} must be before {ComparisonValue}");
+
+            RuleFor(p => p.EndDate)
+                .GreaterThan(p => p.StartDate)
+                .WithMessage("{PropertyName} must be after {ComparisonValue}");
+
+            RuleFor(p => p.LeaveTypeId)
+                .GreaterThan(0)
+                .WithMessage("{PropertyName} does not exist.");
+        }
+    }
+}

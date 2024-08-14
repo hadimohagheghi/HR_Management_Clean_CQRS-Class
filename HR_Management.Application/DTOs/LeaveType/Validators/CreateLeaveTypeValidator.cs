e632a312d033143248de64262c81108335b6e858
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-14 15:46:34 +0330

    نصب و فعالسازی FluentValidation

---
+++ HR_Management.Application/DTOs/LeaveType/Validators/CreateLeaveTypeValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeValidator:AbstractValidator<CreateLeaveTypeDto>
+    {
+        public CreateLeaveTypeValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} Must not Exceed 50");
+
+            RuleFor(p => p.DefaultDay)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1.")
+                .LessThan(100).WithMessage("{PropertyName} must be less than 100.");
+        }
+    }
+}

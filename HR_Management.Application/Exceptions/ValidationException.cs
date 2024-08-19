1d823ae782543243a50d911fe68b8227bac390ce
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-19 17:30:22 +0330

    ساخت Exception های اختصاصی در Asp Core

---
+++ HR_Management.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+
+namespace HR_Management.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+
+        //public ValidationException(string message):base(message)
+        //{
+
+
+        //}
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public ValidationException(ValidationResult validationResult)
+        {
+
+            foreach (var err in validationResult.Errors)
+            {
+                Errors.Add(err.ErrorMessage);
+            }
+        }
+    }
+}

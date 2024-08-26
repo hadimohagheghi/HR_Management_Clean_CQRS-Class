6ad786c4d1d3fc1a46e9d3ff912ee7d934816b4d
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-26 17:20:21 +0330

    تنظیمات Context در EF Core

--- HR_Management.Persistence/LeaveManagementDbContext.cs
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using HR_Management.Domain;
+using HR_Management.Domain.Common;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
 
 namespace HR_Management.Persistence
 {
@@ -14,5 +18,41 @@ namespace HR_Management.Persistence
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }

64a21877ba4e5971f4100fbf4a6ffed0e5e1e4db
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-26 17:06:30 +0330

    ساخت DB Context و تعریف Entity ها

---
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HR_Management.Domain;
+
+namespace HR_Management.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}

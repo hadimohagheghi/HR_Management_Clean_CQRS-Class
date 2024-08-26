8de2bd96fe2ec7afd8e50a2b60c310cf8bffcf34
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-08-26 17:28:17 +0330

    پیاده سازی Generic Repository

--- HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
@@ -9,7 +9,7 @@ namespace HR_Management.Application.Persistence.Contracts
         Task<IReadOnlyList<T>> GetAll();
         Task<bool> Exist(int id);
         Task<T> Add(T entity);
-        Task<T> Update(T entity);
-        Task<T> Delete(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
     }
 }

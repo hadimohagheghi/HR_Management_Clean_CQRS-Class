bcebb49470a8de3cc41ec529954782118fc38f73
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-09-11 17:18:57 +0330

    ساخت پروژه Unit Test و نصب بسته های مورد نیاز

---
+++ HR_Management.Application.UnitTests/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.UnitTests
+{
+    public class Class1
+    {
+        int add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+
+        [Fact]
+        public void AddTest1()
+        {
+            Assert.Equal(4,add(2,2));
+        }
+
+
+
+        [Fact]
+        public void AddTest2()
+        {
+            Assert.Equal(5, add(3, 2));
+        }
+
+
+    }
+}
